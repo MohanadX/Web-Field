ad11c6ff4459c6934da9d6d295daba4139fb4fbf
Mohanad Ayoub <alaqlmhnd3@gmail.com>
2025-10-16 18:29:56 +0300

    added Netlify hoisting config

--- components/StartupForm.tsx
+++ components/StartupForm.tsx
@@ -65,7 +65,7 @@ const StartupForm = () => {
 			};
 		}
 	};
-	const [state, formAction, isPending] = useActionState(handleFormSubmit, {
+	const [, formAction, isPending] = useActionState(handleFormSubmit, {
 		error: "",
 		status: "INITIAL",
 	});

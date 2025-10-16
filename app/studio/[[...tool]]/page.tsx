642604f1a5703cdc032059e8f000ee7ba782346a
Mohanad Ayoub <alaqlmhnd3@gmail.com>
2025-10-16 21:38:40 +0300

    solved sanity studio problems

--- app/studio/[[...tool]]/page.tsx
+++ app/studio/[[...tool]]/page.tsx
@@ -1,5 +1,3 @@
-"use client"; // <-- Important: makes this page a client component
-
 /**
  * This route is responsible for the built-in authoring environment using Sanity Studio.
  * All routes under your studio path is handled by this file using Next.js' catch-all routes:
@@ -11,6 +9,8 @@
 
 import { NextStudio } from "next-sanity/studio";
 import config from "../../../sanity.config";
+
+export { metadata, viewport } from "next-sanity/studio";
 
 export default function StudioPage() {
 	return <NextStudio config={config} />;
